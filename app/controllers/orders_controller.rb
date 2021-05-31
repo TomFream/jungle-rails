5b07a8efdbfa513a4856dea6da871e8f2b86ee5b
Tom Fream <tomfream@gmail.com>
2021-05-31 16:39:51 -0600

    Implements user authentication

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -4,7 +4,7 @@ class OrdersController < ApplicationController
     @order = Order.find(params[:id])  
     @items = LineItem.where(order_id: params[:id])
     # @product = Product.where(id: @items[0][:product_id])
-    @test = Product.line_items
+    # @test = Product.line_items
     # raise @test.inspect
 
    
